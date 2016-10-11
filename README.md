ab039c2b0f69f309b7820a4024b7a7009d50b06c
JacquesFernandes <jacques.fernandes.1337@gmail.com>
2016-10-11 21:02:46 +0530

    updates and made setup.sh with some stuff

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # termux-setup
 Small bash file to set up termux (android) 
+
+- apt update and upgrade is run.
+
+Tools which will be installed (intentionally):
+ - man
+ - curl
+ - nano
+ - git
+ - openssh
+ - coreutils
+ -
---
+++ setup.sh
@@ -0,0 +1,18 @@
+echo " ~~ INITIALIZING ~~ ";
+
+echo "(would use cowsay, but I still need to find the repo for 
+it, will do it soon...)";
+
+#here we go...
+
+echo "";
+apt update;
+echo "\nUpdate completed, installing packages...";
+echo "";
+
+apt install -y man;
+apt install -y curl;
+apt install -y nano;
+apt install -y git;
+apt install -y openssh;
+apt install -y coreutils;
